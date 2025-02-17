13d4f711dd0cd653743b1d60910a08c7f371d828
Yuristam <yuristamasanakunov@gmail.com>
2025-02-17 23:30:51 +0600

    added sql connection

--- CoreBankingSystem.CMD/Menu/CreateUserMenu.cs
+++ CoreBankingSystem.CMD/Menu/CreateUserMenu.cs
@@ -1,10 +1,35 @@
 ﻿namespace CoreBankingSystem.CMD.Menu
 {
-    public class CreateUserMenu
+    public partial class CreateUserMenu
     {
         public static void PrintCreateUserMenu()
         {
+            string userInput;
 
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(
+                    "1. Create Client\n" +
+                    "2. Edit Client \n" +
+                    "3. Delete Client\n" +
+                    "4. back \n");
+
+                userInput = Console.ReadLine().ToLower().Trim();
+
+                switch (userInput)
+                {
+                    case "1": CreateUser(); break;
+                   // case "2": GetUsersByID(); break;
+                    //case "3": GetUsersByPhoneNumber(); break;
+                    case "4": MainMenu.PrintMainMenu(); break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("There is no such command, please enter right command.");
+                        Task.Delay(1000);
+                        break;
+                }
+            }
         }
     }
 }
---
+++ CoreBankingSystem.CMD/MenuCommands/CreateUserMenuCommands.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreBankingSystem.CMD.Menu
+{
+    public partial class CreateUserMenu
+    {
+        public static void CreateUser()
+        {
+
+        }
+    }
+}
